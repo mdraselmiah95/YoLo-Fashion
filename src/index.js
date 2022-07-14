2b270115d5f128db7a17e56282841dbf1c39adf8
mdraselmiah95 <russelroy15@gmail.com>
2022-07-14 10:26:22 +0600

    WIP on YoLo Fashion SCSS  file setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
+
 import Layout from "./components/Layout";
+
+import "./sass/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
