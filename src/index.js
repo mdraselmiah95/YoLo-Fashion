c3848f1a1ac60e43806e1db3c8b38116c71c256e
mdraselmiah95 <russelroy15@gmail.com>
2022-07-14 10:48:15 +0600

    WIP on YoLo Fashion file setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 
@@ -10,9 +11,11 @@ import "./sass/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
