474ac1f6f0473e9810ac3af62764819d8dcdaf5b
mdraselmiah95 <russelroy15@gmail.com>
2022-07-16 15:35:47 +0600

    WIP on YoLo Fashion Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
+import Grid from "./Grid";
+import logo from "../assets/images/Logo-2.png";
+
+const footerAboutLinks = [
+  {
+    display: "Giới thiệu",
+    path: "/about",
+  },
+  {
+    display: "Liên hệ",
+    path: "/about",
+  },
+  {
+    display: "Tuyển dụng",
+    path: "/about",
+  },
+  {
+    display: "Tin tức",
+    path: "/about",
+  },
+  {
+    display: "Hệ thống cửa hàng",
+    path: "/about",
+  },
+];
+
 const Footer = () => {
   return <div>Footer</div>;
 };
