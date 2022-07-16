701126aa074747a232e38dce8e0e0802a64540dc
mdraselmiah95 <russelroy15@gmail.com>
2022-07-16 15:32:08 +0600

    WIP on YoLo Fashion Grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Grid = (props) => {
-  return <div>Grid</div>;
+  const style = {
+    gap: props.gap ? `${props.gap}px` : "0",
+  };
+
+  const col = props.col ? `grid-col-${props.col}` : "";
+  const mdCol = props.mdCol ? `grid-col-md-${props.mdCol}` : "";
+  const smCol = props.smCol ? `grid-col-sm-${props.smCol}` : "";
+  return (
+    <div className={`grid${col}${mdCol}${smCol}`} style={style}>
+      {props.children}
+    </div>
+  );
 };
 
 Grid.propTypes = {
