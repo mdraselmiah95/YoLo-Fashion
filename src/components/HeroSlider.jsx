ecbbd9d743b61ac1f838ce005a24e70cda4c0066
mdraselmiah95 <russelroy15@gmail.com>
2022-07-19 14:31:11 +0600

    WIP on YoLo Fashion HeroSlider Part

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const HeroSlider = ({ data }) => {
   return (
@@ -15,11 +16,35 @@ const HeroSlider = ({ data }) => {
   );
 };
 
-HeroSlider.propTypes = {};
-
-const HeroSliderItem = ({ item }) => {
-  console.log(item);
-  <div className="hero-slider__item"></div>;
+HeroSlider.propTypes = {
+  data: PropTypes.array.isRequired,
 };
 
+const HeroSliderItem = ({ item }) => (
+  <div className={`hero-slider__item`}>
+    <div className="hero-slider__item__info">
+      <div className={`hero-slider__item__info__title color-${item.color}`}>
+        <span>{item.title}</span>
+      </div>
+      <div className="hero-slider__item__info__description">
+        <span>{item.description}</span>
+      </div>
+      <div className="hero-slider__item__info__btn">
+        {/* <Link to={props.item.path}>
+          <Button
+            backgroundColor={props.item.color}
+            icon="bx bx-cart"
+            animate={true}
+          >
+            xem chi tiết
+          </Button>
+        </Link> */}
+      </div>
+    </div>
+    <div className="hero-slider__item__image">
+      <div className={`shape bg-${item.color}`}></div>
+      <img src={item.img} alt="" />
+    </div>
+  </div>
+);
 export default HeroSlider;
