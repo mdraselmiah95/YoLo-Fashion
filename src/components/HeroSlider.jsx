c574ca2952abbd8111329364eb23665f56e6a09f
mdraselmiah95 <russelroy15@gmail.com>
2022-07-19 14:08:46 +0600

    WIP on YoLo Fashion HeroSlider Part

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const HeroSlider = (props) => {
-  return <div>HeroSlider</div>;
+const HeroSlider = ({ data }) => {
+  return (
+    <div className="hero-slider">
+      {data.map((item) => (
+        <HeroSliderItem
+          key={item.id}
+          item={item}
+          // active={index === activeSlide}
+        />
+      ))}
+    </div>
+  );
 };
 
 HeroSlider.propTypes = {};
+
+const HeroSliderItem = ({ item }) => {
+  console.log(item);
+  <div className="hero-slider__item"></div>;
+};
 
 export default HeroSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Helmet from "../components/Helmet";
 import HeroSlider from "../components/HeroSlider";
 
+import heroSliderDate from "../assets/fake-data/hero-slider";
+
 const Home = () => {
   return (
     <Helmet title="Trang chủ">
       {/* hero slider */}
-      <HeroSlider />
+      <HeroSlider data={heroSliderDate} />
+      {/* end hero slider */}
     </Helmet>
   );
 };
