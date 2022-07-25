3a681a270a5aec6aa3610bb5def4d997420f3bd7
mdraselmiah95 <russelroy15@gmail.com>
2022-07-25 23:27:31 +0600

    WIP on YoLo Fashio Product Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import Section, { SectionBody, SectionTitle } from "../components/Section";
 import PolicyCard from "../components/PolicyCard";
 import policy from "../assets/fake-data/policy";
 import productData from "../assets/fake-data/products";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   console.log(productData);
@@ -48,7 +49,18 @@ const Home = () => {
       <Section>
         <SectionTitle>top sản phẩm bán chạy trong tuần</SectionTitle>
         <SectionBody>
-          <Grid col={4} mdCol={2} smCol={1} gap={20}></Grid>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(4).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
         </SectionBody>
       </Section>
       {/* best selling section end*/}
