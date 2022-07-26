8069de3f12f359f18676a22b38cfa5fef2a08001
mdraselmiah95 <russelroy15@gmail.com>
2022-07-26 20:34:40 +0600

    WIP on YoLo Fashion Popular Product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -86,15 +86,35 @@ const Home = () => {
       </Section>
       {/* new arrival section end*/}
 
-      {/* banner */}
+      {/* banner start*/}
       <Section>
         <SectionBody>
           <Link to="/catalog">
-            <img src={banner} alt="" />
+            <img src={banner} alt="banner" />
           </Link>
         </SectionBody>
       </Section>
       {/* end banner */}
+
+      {/* popular product section */}
+      <Section>
+        <SectionTitle>phổ biến</SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(12).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/* end popular product section */}
     </Helmet>
   );
 };
