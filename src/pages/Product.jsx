bc9840d1884d26e6133e3ecad4b3ec4600781fa7
mdraselmiah95 <russelroy15@gmail.com>
2022-07-29 00:55:19 +0600

    WIP on YoLo Fashion Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import productData from "../assets/fake-data/products";
+import Helmet from "../components/Helmet";
+import Section, { SectionBody, SectionTitle } from "../components/Section";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = (props) => {
+  const product = productData.getProductBySlug(props.match.params.slug);
+  return (
+    <Helmet title={product.title}>
+      <Section>
+        <SectionBody>{/* <ProductView product={product} /> */}</SectionBody>
+      </Section>
+      <Section>
+        <SectionTitle>Khám phá thêm</SectionTitle>
+      </Section>
+    </Helmet>
+  );
 };
 
 export default Product;
