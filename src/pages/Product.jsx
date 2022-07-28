84cff052d4599d607c3fca43f31f5285fbf90971
mdraselmiah95 <russelroy15@gmail.com>
2022-07-29 00:57:27 +0600

    WIP on YoLo Fashion Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import productData from "../assets/fake-data/products";
+import Grid from "../components/Grid";
 import Helmet from "../components/Helmet";
+import ProductCard from "../components/ProductCard";
 import Section, { SectionBody, SectionTitle } from "../components/Section";
 
 const Product = (props) => {
   const product = productData.getProductBySlug(props.match.params.slug);
+
+  const relatedProducts = productData.getProducts(8);
   return (
     <Helmet title={product.title}>
       <Section>
@@ -12,6 +16,20 @@ const Product = (props) => {
       </Section>
       <Section>
         <SectionTitle>Khám phá thêm</SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {relatedProducts.map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
       </Section>
     </Helmet>
   );
