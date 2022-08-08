0f437197ac8075ef08c0258ae70f334117f8e470
mdraselmiah95 <russelroy15@gmail.com>
2022-08-08 23:58:08 +0600

    old code added

--- src/components/Helmet.jsx
+++ src/components/Helmet.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const Helmet = (props) => {
-  document.title = "Yolo - " + props.title;
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return <div>{props.children}</div>;
-};
+const Helmet = props => {
+
+    document.title = 'Yolo - ' + props.title
+
+    React.useEffect(() => {
+        window.scrollTo(0,0)
+    }, [])
+
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
 
 Helmet.propTypes = {
-  title: PropTypes.string.isRequired,
-};
+    title: PropTypes.string.isRequired
+}
 
-export default Helmet;
+export default Helmet
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Routes from "../routes/Routes";
-import Footer from "./Footer";
-import Header from "./Header";
+import React from 'react'
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './Header'
+import Footer from './Footer'
+import ProductViewModal from './ProductViewModal'
+
+import Routes from '../routes/Routes'
 
 const Layout = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <div className="container">
-          <div className="main">
-            <Routes />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
-};
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <div>
+                    <Header {...props}/>
+                    <div className="container">
+                        <div className="main">
+                            <Routes/>
+                        </div>
+                    </div>
+                    <Footer/>
+                    <ProductViewModal/>
+                </div>
+            )}/>
+        </BrowserRouter>
+    )
+}
 
-export default Layout;
+export default Layout
