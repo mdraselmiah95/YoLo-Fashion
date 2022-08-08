d0dbdede6754dbd3795a56d0c6790ca33b9d8e59
mdraselmiah95 <russelroy15@gmail.com>
2022-08-08 23:59:39 +0600

    old code added pages

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,46 +1,58 @@
-import React from "react";
-import productData from "../assets/fake-data/products";
-import Grid from "../components/Grid";
-import Helmet from "../components/Helmet";
-import ProductCard from "../components/ProductCard";
-import ProductView from "../components/ProductView";
-import Section, { SectionBody, SectionTitle } from "../components/Section";
+import React from 'react'
 
-const Product = (props) => {
-  const product = productData.getProductBySlug(props.match.params.slug);
+import Helmet from '../components/Helmet'
+import Section, {SectionBody, SectionTitle} from '../components/Section'
+import Grid from '../components/Grid'
+import ProductCard from '../components/ProductCard'
+import ProductView from '../components/ProductView'
 
-  const relatedProducts = productData.getProducts(8);
+import productData from '../assets/fake-data/products'
 
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [product]);
+const Product = props => {
 
-  return (
-    <Helmet title={product.title}>
-      <Section>
-        <SectionBody>
-          <ProductView product={product} />
-        </SectionBody>
-      </Section>
-      <Section>
-        <SectionTitle>Khám phá thêm</SectionTitle>
-        <SectionBody>
-          <Grid col={4} mdCol={2} smCol={1} gap={20}>
-            {relatedProducts.map((item, index) => (
-              <ProductCard
-                key={index}
-                img01={item.image01}
-                img02={item.image02}
-                name={item.title}
-                price={Number(item.price)}
-                slug={item.slug}
-              />
-            ))}
-          </Grid>
-        </SectionBody>
-      </Section>
-    </Helmet>
-  );
-};
+    const product = productData.getProductBySlug(props.match.params.slug)
 
-export default Product;
+    const relatedProducts = productData.getProducts(8)
+
+    React.useEffect(() => {
+        window.scrollTo(0,0)
+    }, [product])
+
+    return (
+        <Helmet title={product.title}>
+            <Section>
+                <SectionBody>
+                    <ProductView product={product}/>
+                </SectionBody>
+            </Section>
+            <Section>
+                <SectionTitle>
+                    Khám phá thêm
+                </SectionTitle>
+                <SectionBody>
+                    <Grid
+                        col={4}
+                        mdCol={2}
+                        smCol={1}
+                        gap={20}
+                    >
+                        {
+                            relatedProducts.map((item, index) => (
+                                <ProductCard
+                                    key={index}
+                                    img01={item.image01}
+                                    img02={item.image02}
+                                    name={item.title}
+                                    price={Number(item.price)}
+                                    slug={item.slug}
+                                />   
+                            ))
+                        }
+                    </Grid>
+                </SectionBody>
+            </Section>
+        </Helmet>
+    )
+}
+
+export default Product
